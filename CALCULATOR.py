b15428a150e7310d31d47ee9ffb9868fd5d8813d
Stefo16 <76417700+Stefo16@users.noreply.github.com>
2021-08-24 01:41:29 -0400

    Add files via upload

---
+++ CALCULATOR.py
@@ -0,0 +1,14 @@
+num1 = input("Enter the amount you are investing: ")
+num2 = input("Enter the stock price: ")
+
+result = float(num1) / float(num2)
+
+print(result)
+
+num3 = input("Enter the amount of shares: ")
+num4 = input("Enter the stock price: ")
+
+dollar_amount = float(num3) * float(num4)
+
+print(dollar_amount)
+print("Do you have another stock?")
